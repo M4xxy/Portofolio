e8aaa97d6199ac432a9b1cc8dc148ebde912eaa1
Maxwell <kevinmaxwell205@gmail.com>
2025-06-30 00:54:23 +0700

    Initial commit

---
+++ js/script-porto.js
@@ -0,0 +1,15 @@
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+            aside = document.querySelector(".aside");
+            navTogglerBtn.addEventListener("click", () =>
+            {
+                asideSectionTogglerBtn();
+            })
+
+            function asideSectionTogglerBtn()
+            {
+              aside.classList.toggle("open");
+              navTogglerBtn.classList.toggle("open");
+              for(let i=0; i<totalSection ; i++){
+                allSection[i].classList.toggle("open");
+              }
+            }   
